19580d350ba6b2aab066534c4d2c3de2e0d017b5
li.bojing <1325349854@qq.com>
2024-03-18 20:29:26 +0800

    添加demo-hub 和basic（配置了maven）

---
+++ code-generate-basic/src/main/java/com/basic/Main.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+//    for(int i =1; i < 6; i++){
+//        System.out.println(x);
+//    }
+}
