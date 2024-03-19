c34d3dfed58c33030e2f41760110a296ee55fdfb
li.bojing <1325349854@qq.com>
2024-03-19 18:20:18 +0800

    DynamicGenerator 完成

---
+++ code-generate-basic/src/main/java/com/basic/model/MainTemplateConfig.java
@@ -0,0 +1,14 @@
+package com.basic.model;
+
+import lombok.Data;
+
+@Data
+public class MainTemplateConfig {
+    /*
+     * 1. 在代码开头增加作者
+     * 2. 修改程序的可输出信息
+     * 3. 将循环设置为可选*/
+    private String author = "LBJ";
+    private String outputText = "end:";
+    private boolean loop = true;
+}
--- code-generate-basic/src/test/java/FreeMarkerTest.java
+++ code-generate-basic/src/test/java/FreeMarkerTest.java
@@ -1,2 +1,50 @@
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FreeMarkerTest {
+    @Test
+    public  void  test() throws IOException, TemplateException {
+        // new 出 Configuration 对象，参数为 FreeMarker 版本号
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
+
+        // 指定模板文件所在的路径
+        configuration.setDirectoryForTemplateLoading(new File("G:\\个人助手\\code-generation\\code-generate-basic\\src\\main\\resources\\templete "));
+
+        // 设置模板文件使用的字符集
+        configuration.setDefaultEncoding("utf-8");
+
+        Template template = configuration.getTemplate("myweb.html.ftl");
+
+        // 创建数据模型
+        Map<String, Object> dataModel = new HashMap<>();
+        dataModel.put("currentYear", 2024);
+        List<Map<String, Object>> menuItems = new ArrayList<>();
+        Map<String, Object> menuItem1 = new HashMap<>();
+        menuItem1.put("url", "https://mofanh.github.io/web-portfolio/");
+        menuItem1.put("label", "LBJ web");
+        Map<String, Object> menuItem2 = new HashMap<>();
+        menuItem2.put("url", "https://github.com");
+        menuItem2.put("label", "LBJ Github");
+        menuItems.add(menuItem1);
+        menuItems.add(menuItem2);
+        dataModel.put("menuItems", menuItems);
+
+        // 生成
+        Writer out = new FileWriter("myweb.html");
+        template.process(dataModel, out);
+
+        // 生成文件后关闭
+        out.close();
+    }
 }
