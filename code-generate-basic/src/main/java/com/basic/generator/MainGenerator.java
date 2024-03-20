6bf4a12adf89ffb6293cff53fc5425079f7b7c26
li.bojing <1325349854@qq.com>
2024-03-20 11:57:50 +0800

    动态+静态文件生成整合完毕

---
+++ code-generate-basic/src/main/java/com/basic/generator/MainGenerator.java
@@ -0,0 +1,38 @@
+package com.basic.generator;
+
+import com.basic.model.MainTemplateConfig;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainGenerator {
+    public static void main(String[] args) throws TemplateException, IOException {
+        // 创建数据模型
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("LBJ");
+        mainTemplateConfig.setLoop(true);
+        mainTemplateConfig.setOutputText("LBJ`s output");
+
+        doGenerate(mainTemplateConfig);
+    }
+
+    public static void doGenerate(Object model) throws TemplateException, IOException {
+        String projectPath = System.getProperty("user.dir");
+        File parentFile = new File(projectPath).getParentFile();
+        System.out.println(parentFile);
+
+//        String src = parentFile + File.separator + "code-generate-demo-hub\\acm-template" + File.separator + "acm-template";
+        String inputStaticDirPath = new File(parentFile, "code-generate-demo-hub\\acm-template" + File.separator + "acm-template").getAbsolutePath();
+        String outputStaticDirPath = projectPath;
+
+//        静态文件生成
+        StaticGenerator.copyFilesByRecursive(inputStaticDirPath, outputStaticDirPath);
+
+//        动态文件生成
+//       code-generate-basic/acm-template/src/com/yupi/acm/MainTemplate.java
+        String inputDynamicFilePath = projectPath + File.separator + "src\\main\\resources\\templete\\MainTemplate.java.ftl";
+        String outputDynamicFilePath = projectPath + File.separator + "acm-template\\src\\com\\yupi\\acm\\MainTemplate.java";
+        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+    }
+}
